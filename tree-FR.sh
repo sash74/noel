f90ad98ec29c4dcb23a2bd3f5113d97588b1cc78
sash74 <118597640+sash74@users.noreply.github.com>
2022-12-30 15:24:43 +0100

    Update tree-FR.sh

--- tree-FR.sh
+++ tree-FR.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+ = $espace
 trap "tput reset; tput cnorm; exit" 2
 clear
 tput civis
@@ -32,14 +33,14 @@ for ((i=1; i<=2; i++))
 new_year=$(date +'%Y')
 let new_year++
 tput setaf 1; tput bold
-tput cup $lin $((c - 6)); echo JOYEUX NOEL !!
-tput cup $((lin + 1)) $((c - 13)); echo Et beaucoup de CODE en $new_year
+tput cup $lin $((c - 6)); echo JOYEUX NOEL !!!
+tput cup $((lin + 1)) $((c - 12));echo Et beaucoup d          echo  $espace          _en  $new_year
 let c++
 k=1
 
 # Lights and decorations
 while true; do
-    for ((i=1; i<=35; i++)) {
+    for ((i=1; i<=10; i++)) {
         # Turn off the lights
         [ $k -gt 1 ] && {
             tput setaf 2; tput bold
@@ -58,12 +59,12 @@ while true; do
         color=$(((color+1)%8))
         # Flashing text
         sh=1
-        for l in C O D E
+        for l in   a m o u r 
         do
             tput cup $((lin+1)) $((c+sh))
             echo $l
             let sh++
-            sleep 0.01
+            sleep 0.0089
         done
     }
     k=$((k % 2 + 1))
